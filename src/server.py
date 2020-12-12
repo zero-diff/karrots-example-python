3a61538dacce96d9c9b3c547e28d41d5d8ebd6f2
Jeff Williams <jeff.williams@gadgetworks.com>
2020-12-12 12:10:24 -0800

    supports pylint and fixes problems found
    #3

--- src/server.py
+++ src/server.py
@@ -1,29 +1,33 @@
-# Example Karrots Python application
-#
-# It demonstrates two ways to handle data processing:
-# 1: periodic background jobs that might make http calls
-# 2: http server that handles inbound http requests
+"""
+  example karrots python application
+
+  this example demonstrates two ways to handle data processing:
+  1: periodic background jobs that might make http calls
+  2: http server that handles inbound http requests
+"""
 
 import logging.config
 
 from flask import Flask, render_template
 
-import src.background
+import background
 
 service = Flask(__name__)
 
 
 @service.route("/")
 def home():
+    """ handle root route """
     return render_template('index.html')
 
 
 def setup_service():
+    """ service initialization """
     logging.config.fileConfig('logging_config.ini')
 
     logger = logging.getLogger(__name__)
-
-    src.background.setupScheduler(service)
+    background.setup_scheduler(service)
+    logger.debug("setup complete")
 
     return service
 
